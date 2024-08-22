987f981a9f432a4814641b37dbb6a31015b5e60c
Karthik V <97800273+KarthikVeeranala@users.noreply.github.com>
2024-08-22 20:34:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById('giftButton').addEventListener('click', function() {
+    document.getElementById('page1').style.display = 'none';
+    document.getElementById('page2').style.display = 'block';
+});
